85f117a6dc9df65f043bf30ae4c28c86e0649642
Nahum Torres <erick.natg@gmail.com>
2023-03-05 10:55:40 -0600

    Bases de webSockets con Node

---
+++ models/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require('cors');
+const { socketController } = require("../sockets/controller");
+
+class Server {
+  
+    constructor() {
+    this.app = express(); //Le asignamos a una propiedad express, es como instanciar pero ahora usamos App
+    this.port = process.env.PORT;
+    this.server = require('http').createServer( this.app );
+    this.io = require('socket.io')( this.server );
+
+    this.paths = {}
+
+    //Middlewares
+    this.middlewares();
+
+    //Rutas de mi aplicación
+    this.routes();
+
+    //Sockets
+    this.sockets();
+  }
+
+  middlewares(){
+    
+    //CORS
+    this.app.use(cors());
+
+    //Directorio publico
+    this.app.use( express.static('public'));
+
+  }
+
+  routes() {
+    //this.app.use(this.paths.auth, require('../routes/auth'));
+  }
+
+  sockets(){
+
+    this.io.on('connection',socketController)
+  }
+
+  listen(){
+    this.server.listen(this.port,()=>{
+        console.log("Esta corriendo en el puerto", this.port);
+    });
+  }
+}
+
+module.exports = Server;
